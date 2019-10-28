cd2d00bed98367ce4f562e40adbabc945be80518
Mariia <maria.bookvoed@gmail.com>
2019-10-28 16:44:45 -0300

    delete from component, up state

--- src/components/counterList/index.js
+++ src/components/counterList/index.js
@@ -1,14 +1,67 @@
 import React, { Component, Fragment } from 'react';
-import Page from './page'
+import './styles.css';
+import Counter from '../counter/index'
+// import Page from './page'
 
 class CounterList extends  Component {
+    constructor(props){
+        super(props);
+        this.state={
+            counters:[]
+        }
+        this.showCounterList=this.showCounterList.bind(this)
+        this.deleteCounter=this.deleteCounter.bind(this)
+    }
+    componentDidMount(){
+        fetch(`http://localhost:3000/api/v1/counters`)
+        .then(response=> response.json())
+        .then(data =>this.setState({counters:data}))
+        .catch(error=>console.log('parsing failed', error))
+      }
+    showCounterList(){
+        return(
+            this.state.counters.map((counter, index)=>{
+                return(
+                <Counter id={counter.id} deleteCounter={this.deleteCounter} count={counter.count} names={counter.title} key={counter.id}/>
+                )
+            })
+        )
+    }
+    deleteCounter(idCounter){
+        // delete from api
+        fetch('http://localhost:3000/api/v1/counter', {
+            method: 'DELETE',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({id: idCounter})
+            })
+            .then(res => res.text()) // OR res.json()
+            .then(res => console.log(res))
+         // delete from component
+            let newCounters = [...this.state.counters];
+            for( let i = 0; i<newCounters.length; i++){
+                if ( newCounters[i].id === idCounter){
+                    newCounters.splice(i, 1);
+                   
+                }
+            }
+            this.setState({counters:newCounters});   
+    }
     
     render(){
-        return(
-            
-                <Page/>
-          
-        )
+        console.log(this.state.counters)
+        return (
+     
+            <div className="counter-list container">
+                <div>
+                     <p>
+                         Team List:
+                     </p>
+                 </div>
+                    {this.showCounterList()}
+                 </div>
+           
+       
+         )
     }
 }
 export default CounterList;
--- src/components/counterList/page.js
+++ src/components/counterList/page.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import './styles.css';
-import Counter from '../counter/index'
-class Page extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            counters:[]
-        }
-        this.showCounterList=this.showCounterList.bind(this)
-        this.deleteCounter=this.deleteCounter.bind(this)
-    }
-    componentDidMount(){
-        fetch(`http://localhost:3000/api/v1/counters`)
-        .then(response=> response.json())
-        .then(data =>this.setState({counters:data}))
-        .catch(error=>console.log('parsing failed', error))
-      }
-    showCounterList(){
-        return(
-            this.state.counters.map((counter, index)=>{
-                return(
-                <Counter id={counter.id} deleteCounter={this.deleteCounter} count={counter.count} names={counter.title} key={counter.id}/>
-                )
-            })
-        )
-    }
-    deleteCounter(idCounter){
-        fetch('http://localhost:3000/api/v1/counter', {
-            method: 'DELETE',
-            headers: {'content-type': 'application/json'},
-            body: JSON.stringify({id: idCounter})
-            })
-            .then(res => res.text()) // OR res.json()
-            .then(res => console.log(res))
-    }
-    render(){
-        console.log(this.state.counters)
-    return (
+// import './styles.css';
+// import Counter from '../counter/index'
+// class Page extends Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state={
+    //         counters:[]
+    //     }
+    //     this.showCounterList=this.showCounterList.bind(this)
+    //     this.deleteCounter=this.deleteCounter.bind(this)
+    // }
+    // componentDidMount(){
+    //     fetch(`http://localhost:3000/api/v1/counters`)
+    //     .then(response=> response.json())
+    //     .then(data =>this.setState({counters:data}))
+    //     .catch(error=>console.log('parsing failed', error))
+    //   }
+    // showCounterList(){
+    //     return(
+    //         this.state.counters.map((counter, index)=>{
+    //             return(
+    //             <Counter id={counter.id} deleteCounter={this.deleteCounter} count={counter.count} names={counter.title} key={counter.id}/>
+    //             )
+    //         })
+    //     )
+    // }
+    // deleteCounter(idCounter){
+    //     fetch('http://localhost:3000/api/v1/counter', {
+    //         method: 'DELETE',
+    //         headers: {'content-type': 'application/json'},
+    //         body: JSON.stringify({id: idCounter})
+    //         })
+    //         .then(res => res.text()) // OR res.json()
+    //         .then(res => console.log(res))
+    // }
+    // render(){
+        // console.log(this.state.counters)
+    // return (
      
-       <div className="counter-list container">
-           <div>
-                <p>
-                    Team List:
-                </p>
-            </div>
-               {this.showCounterList()}
-            </div>
+    //    <div className="counter-list container">
+    //        <div>
+    //             <p>
+    //                 Team List:
+    //             </p>
+    //         </div>
+    //            {this.showCounterList()}
+    //         </div>
       
   
-    )
-}
-}
-export default Page;
+    // )
+// }
+// }
+// export default Page;

bbf1a1015106a3d9b10e1e8bbfa2606ea5c46b2c
Mariia <maria.bookvoed@gmail.com>
2019-10-29 23:54:40 -0300

    add:pikachu

--- src/components/appBar/page.js
+++ src/components/appBar/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Pika from '../../img/pika.png'
 import './styles.css'
 
 function Page(props){
@@ -10,6 +11,7 @@ function Page(props){
     return (
        <AppBar style={{background:'black'}} position='sticky'>
            <Toolbar className="appBar">
+               <img src={Pika}/>
             <Typography variant="h6">
                 Team Count
             </Typography>   

68d5f73116bc8e0dac9fe372f8c44e61f702472f
Mariia <maria.bookvoed@gmail.com>
2019-10-29 23:55:20 -0300

    Updates

---
+++ precache-manifest.38b9d81aed5f54da30f56767f37e6e7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4ed45ac033e01767e1c57e6a5e548a",
+    "url": "/counter-project-cornershop/index.html"
+  },
+  {
+    "revision": "dfd4ccde3fdfd0c98c8b",
+    "url": "/counter-project-cornershop/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "1f9bbc85e9754113a1a7",
+    "url": "/counter-project-cornershop/static/css/main.71c5b503.chunk.css"
+  },
+  {
+    "revision": "dfd4ccde3fdfd0c98c8b",
+    "url": "/counter-project-cornershop/static/js/2.2f5e59bd.chunk.js"
+  },
+  {
+    "revision": "1f9bbc85e9754113a1a7",
+    "url": "/counter-project-cornershop/static/js/main.b827ddf0.chunk.js"
+  },
+  {
+    "revision": "ab09dd506f89b6f5ef15",
+    "url": "/counter-project-cornershop/static/js/runtime-main.f9aeb49e.js"
+  },
+  {
+    "revision": "28d4a3f52a6c23336ebc325dabc75727",
+    "url": "/counter-project-cornershop/static/media/pika.28d4a3f5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/counter-project-cornershop/precache-manifest.5ff29ff3856a092269d2db950b69ba25.js"
+  "/counter-project-cornershop/precache-manifest.38b9d81aed5f54da30f56767f37e6e7a.js"
 );
 
 self.addEventListener('message', (event) => {

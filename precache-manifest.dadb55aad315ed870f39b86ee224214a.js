35872440e93bc26772a4a1d7f35c47ccf22d167b
Mariia <maria.bookvoed@gmail.com>
2019-11-01 01:03:44 -0300

    Updates

--- precache-manifest.dadb55aad315ed870f39b86ee224214a.js
+++ precache-manifest.dadb55aad315ed870f39b86ee224214a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98ace62c50d6355977ba0c4ca2d3a6a5",
+    "revision": "9d330e56d32f281e2aad9feeb7039e0f",
     "url": "/counter-project-cornershop/index.html"
   },
   {
-    "revision": "321469b628a8c7c50cf8",
+    "revision": "5d312a05611bae7cd453",
     "url": "/counter-project-cornershop/static/css/2.510409cd.chunk.css"
   },
   {
-    "revision": "47dd625ba801d1d80839",
-    "url": "/counter-project-cornershop/static/css/main.0b371e72.chunk.css"
+    "revision": "e334c807e2fb4ff6f59e",
+    "url": "/counter-project-cornershop/static/css/main.2886069a.chunk.css"
   },
   {
-    "revision": "321469b628a8c7c50cf8",
-    "url": "/counter-project-cornershop/static/js/2.74bac636.chunk.js"
+    "revision": "5d312a05611bae7cd453",
+    "url": "/counter-project-cornershop/static/js/2.aa9925a5.chunk.js"
   },
   {
-    "revision": "47dd625ba801d1d80839",
-    "url": "/counter-project-cornershop/static/js/main.e1f08f69.chunk.js"
+    "revision": "e334c807e2fb4ff6f59e",
+    "url": "/counter-project-cornershop/static/js/main.1cea2639.chunk.js"
   },
   {
     "revision": "ab09dd506f89b6f5ef15",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/counter-project-cornershop/precache-manifest.270e1b0ac58c128203596246836e562e.js"
+  "/counter-project-cornershop/precache-manifest.dadb55aad315ed870f39b86ee224214a.js"
 );
 
 self.addEventListener('message', (event) => {
